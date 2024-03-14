22d22e5f876451cc69d9c93e36e57c4a217f7226
eliasb03 <eliasbowmanschool@gmail.com>
2024-03-13 21:43:01 -0700

    Initial commit of the 00_setup_v0 initial rscript, just commiting the script to its ther, no actually code in the script other than the header so far

---
+++ scripts/00_setup_v0.R
@@ -0,0 +1,8 @@
+#------------------------------
+# Biol 406 Project
+# By: Elias, Rebecca, Sara, Sahar 
+# 2024-03-13
+# Description: Initial Rscript for the setup of the BIOL 406 Lab project
+# This script should import packages, prepare functions, and import data 
+#------------------------------
+
